bc00458f3f1069418d1df37e41489c1c31e9dbd4
Neelansh Mathur <neelansh15@outlook.com>
2020-04-25 13:50:41 +0530

    Added Calculator componenet and CSS grid

---
+++ src/components/Calculator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="calculator">
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.calculator{
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
+}
+</style>
